89c0a14c71716195128b1cc22c81dfc0593f3e53
Franco Navarro <frnavarro@itba.edu.ar>
2022-07-10 15:37:27 -0300

    Analysis Finished

--- App.java
+++ App.java
@@ -5,6 +5,8 @@ public class App {
     public static void main(String[] args) {
         runEj4();
         // runUniverse();
+        // runEj1_2();
+        // runEj3();
     }
 
 
@@ -18,10 +20,10 @@ public class App {
     }
 
     public static void runEj1_2(){
-        double d1 = 0.14;
+        double d1 = 0.15;
         double d2 = 0.17;
-        double d3 = 0.20;
-        double d4 = 0.23;
+        double d3 = 0.19;
+        double d4 = 0.21;
         double[] ds = {d1,d2,d3,d4};
         // double dt = (0.1 * Math.sqrt(0.01/100000));
         double dt = 0.00001;
@@ -36,10 +38,10 @@ public class App {
     }
 
     public static void runEj3(){
-        double d1 = 0.14;
+        double d1 = 0.15;
         double d2 = 0.17;
-        double d3 = 0.20;
-        double d4 = 0.23;
+        double d3 = 0.19;
+        double d4 = 0.21;
         double[] ds = {d1,d2,d3,d4};
         // double dt = (0.1 * Math.sqrt(m/kn));
         double dt = 0.00001;
@@ -55,7 +57,7 @@ public class App {
 
     public static void runEj4(){
         double dt = 0.00001;
-        double ktm2 = 1;
+        double ktm2 = 0.5;
         double ktm3 = 2;
         double ktm4 = 3;
         double[] ktms = {ktm2,ktm3,ktm4};
